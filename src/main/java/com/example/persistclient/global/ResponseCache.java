8d8318bc341c61e7d2853a370b29aeb722f5b54e
Tae Soo Kim <taesoo.kim0412@gmail.com>
2021-07-31 22:28:23 -0700

    0731 - :]

--- src/main/java/com/example/persistclient/global/ResponseCache.java
+++ src/main/java/com/example/persistclient/global/ResponseCache.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 public class ResponseCache {
     public static StatusResponse SuccessResponse = new StatusResponse(Constants.SUCCESS);
 //    public static byte[] SuccessResponseBytesUTF = SuccessResponse.toString().getBytes(StandardCharsets.UTF_8);
-    public static byte[] SuccessResponseBytes = ("{ \"status\": " + "\"success\"}").getBytes();
+    public static byte[] SuccessResponseBytes = ("{ \"status\": " + "\"success\" }").getBytes();
+    public static byte[] FailResponseBytes = ("{ \"status\": " + "\"fail\"}").getBytes();
 
 }
--- src/main/java/com/example/persistclient/services/PersistService.java
+++ src/main/java/com/example/persistclient/services/PersistService.java
@@ -5,30 +5,97 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Function;
 
 @Service
 public class PersistService {
+    static Function[] responses = new Function[1];
 
     public static StreamingResponseBody getPersistStream() {
+        try {
+            setResponsesToSuccess();
+            setAfterDelay(10000);
+            StreamingResponseBody responseBody = out -> {
+                while (true) {
+                    responses[0].apply(out);
+                }
+            };
+
+            return responseBody;
+        } catch (Exception e) {
+
+        }
         return out -> {
-            while (true) {
-                System.out.println("Asd");
+        };
+    }
+
+    public static void setAfterDelay(long delay) {
+        try {
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        setResponsesToFail();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, delay); // 300 is the delay in millis
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setResponsesToSuccess() throws IOException {
+        try {
+            responses[0] = out -> {
+                OutputStream newOut = (OutputStream) out;
+//            System.out.println("Asd");
                 //doesnt work with java classes
 //                try (ObjectOutputStream outputClass = new ObjectOutputStream(out)) {
 //                    outputClass.writeObject(ResponseCache.SuccessResponseBytes);
 //                }
-                out.write(ResponseCache.SuccessResponseBytes);
-                out.flush();
-
                 try {
-//                    Thread.sleep(Constants.thirtyMinutesMillis);
+                    newOut.write(ResponseCache.SuccessResponseBytes);
+                    newOut.flush();
+//              Thread.sleep(Constants.thirtyMinutesMillis);
                     Thread.sleep(3000);
 
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            }
-        };
+                return null;
+            };
+        } catch (Exception e) {
+        }
+    }
+
+    public static void setResponsesToFail() throws IOException {
+        try {
+            responses[0] = out -> {
+                OutputStream newOut = (OutputStream) out;
+//            System.out.println("Asd");
+                //doesnt work with java classes
+//                try (ObjectOutputStream outputClass = new ObjectOutputStream(out)) {
+//                    outputClass.writeObject(ResponseCache.SuccessResponseBytes);
+//                }
+                try {
+                    newOut.write(ResponseCache.FailResponseBytes);
+                    newOut.flush();
+//              Thread.sleep(Constants.thirtyMinutesMillis);
+                    Thread.sleep(3000);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            };
+        } catch (Exception e) {
+        }
     }
 }
